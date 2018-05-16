af4d9580ccd380bd1d79aa28b65d3b4886beddf7
Brandon Ireland <workflow@brandons-MacBook-Pro.local>
2018-05-16 11:40:57 +0200

    Mock up Commit

---
+++ index.php
@@ -0,0 +1,330 @@
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>TemecuTech Computers</title>
+        <meta name="description" content="Temecula and Menifee Valley's #1 computer network company."/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/css.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
+        <link href="http://fonts.googleapis.com/css?family=Oswald:300,400,600,700" rel="stylesheet" type="text/css">
+        
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="/favicons/manifest.json">
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="/favicons/favicon.ico">
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+    </head>
+
+    <body class="scroll-assist btn-rounded">
+				
+		<div class="nav-container"><nav class="absolute transparent">
+		        <div class="nav-bar">
+		            <div class="module left">
+		                <a href="index.html">
+		                    <div class="vnu"><img class="logo logo-light" alt="Temecutech" src="img/logo2.png"></div>
+		                    <div class="vnu"><img class="logo logo-dark" alt="Temecutech" src="img/logo2.png"></div>
+		                </a>
+		            </div>
+                    <div class="module left">
+		                <i class="ti-email">&nbsp;</i>
+		                <span class="sub">info@temecutech.com</span>
+		            </div><div class="module left vpf">
+		                <i class="fa fa-phone">&nbsp;</i>
+		                <span class="sub">(951) 377-1133</span>
+		            </div>
+		            <div class="module widget-handle mobile-toggle right visible-sm visible-xs">
+		                <i class="ti-menu"></i>
+		            </div>
+		            <div class="module-group right">
+		                <div class="module left">
+		                    <ul class="menu">
+		                        <li>
+		                            <a href="#services">Services</a>
+		                        </li><li class="vpf">
+		                            <a href="#testimonials">testimonials </a>
+		                        </li><li class="vpf">
+		                            <a href="#recent-news">recent news&nbsp;</a>
+		                        </li><li class="vpf">
+		                            <a href="#contact-us">contact us&nbsp;</a>
+		                        </li>
+		                        
+		                        
+		                    </ul>
+		                </div>
+		                
+		                
+		            </div>
+		            
+		        </div>
+            </nav>
+		
+		</div>
+		
+		<div class="main-container">
+					
+			
+		<section class="cover fullscreen image-bg overlay parallax">
+		        <div class="background-image-holder">
+		            <img alt="background-image" class="background-image" src="img/hero2.jpeg">
+		        </div>
+		        <div class="container v-align-transform">
+		            <div class="row">
+		                <div class="col-sm-12 text-center">
+		                    <h1 class="large uppercase mb16"><strong>Temecutech Computers</strong></h1>
+                            <h5 class="uppercase mb0"><strong>Temecula and menifee valley's number 1 computer network company</strong></h5>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		        <div class="align-bottom relative-xs text-center">
+		            <a class="btn btn-filled btn-lg mb32 mt-xs-40" href="#contact-us">Contact us</a>
+		        </div>
+		    </section>
+            <section>
+		        <div class="container services" id="services">
+		            <div class="row">
+		                <div class="col-sm-10 col-sm-offset-1 text-center">
+		                    <h3 class="uppercase color-primary mb40 mb-xs-24">we fix your computer problems</h3>
+		                    <p class="lead"><strong>Don't trust a squad of geeks.</strong> We provide low-cost, high-value, flat-rate services. Temecutech is there to fix your business' computer, network and web problems, so you can focus on whats important.</p>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="bg-secondary">
+		        <div class="container">
+		            <div class="row v-align-children">
+		                <div class="col-md-7 col-sm-6 text-center mb-xs-24">
+		                    <img class="cast-shadow" alt="Screenshot" src="img/comprepair.jpg">
+		                </div>
+		                <div class="col-md-4 col-md-offset-1 col-sm-5 col-sm-offset-1">
+		                    <h3>Computer Repair</h3>
+                            <p><strong>Full service Provider</strong></p>
+		                      <ul>
+                                <li><i class="fa fa-check-circle-o"></i> Low Cost, Onsite and Remote Service and repairs</li>
+                                <li><i class="fa fa-check-circle-o"></i> Computer and Network Installations</li>
+                                <li><i class="fa fa-check-circle-o"></i> Backup Solutions</li>
+                                <li><i class="fa fa-check-circle-o"></i> Anti-Virus</li>
+                            </ul>
+		                    
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="">
+		        <div class="container">
+		            <div class="row v-align-children">
+		                <div class="col-md-4 col-sm-5 mb-xs-24">
+		                    <h3>Monthly Maintenance</h3>
+		                    <p><strong>All you can eat monthly service</strong></p>
+		                      <ul>
+                                <li><i class="fa fa-check-circle-o"></i> Flat Rate Monthly Monitoring</li>
+                                <li><i class="fa fa-check-circle-o"></i> Web Consulting</li>
+                                <li><i class="fa fa-check-circle-o"></i> Monthly Maintenance</li>
+                            </ul>
+                            <a class="btn-filled btn" href="#contact-us">Contact us</a>
+		                </div>
+		                <div class="col-md-7 col-md-offset-1 col-sm-6 col-sm-offset-1 text-center">
+		                    <img class="cast-shadow" alt="Screenshot" src="img/maintenance.jpeg">
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section><section class="bg-secondary">
+		        <div class="container">
+		            <div class="row v-align-children">
+		                <div class="col-md-7 col-sm-6 text-center mb-xs-24">
+		                    <img class="cast-shadow" alt="Screenshot" src="img/cloud.jpg">
+		                </div>
+		                <div class="col-md-4 col-md-offset-1 col-sm-5 col-sm-offset-1">
+		                    <h3>Cloud Services</h3>
+		                    <p><strong>Hosting with intent </strong></p>
+		                      <ul>
+                                <li><i class="fa fa-check-circle-o"></i> Microsoft Office 365 solutions</li>
+                                <li><i class="fa fa-check-circle-o"></i> Google Apps</li>
+                                <li><i class="fa fa-check-circle-o"></i> Disaster Recovery & backups</li>
+                                <li><i class="fa fa-check-circle-o"></i> Cloud Servers and application hosting</li>
+                            </ul>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section>
+            <section class="image-bg overlay parallax testimonials" id="testimonials">
+		        <div class="background-image-holder">
+		            <img alt="image" class="background-image image-bottom" src="img/testimonial.jpeg">
+		        </div>
+		        <div class="container">
+		            <div class="row mb40 mb-xs-24">
+		                <div class="col-sm-12 text-center spread-children">
+		                    <img class="image-xs testi" alt="testimonial-person2" src="img/test2.jpg">
+		                    <img class="image-xs testi" alt="testimonial-person3" src="img/test3.jpg">
+		                    <img class="image-xs testi" alt="testimonial-person1" src="img/test1.jpg">
+		                </div>
+		            </div>
+		            
+		            <div class="row mb16 mb-xs-0">
+		                <div class="col-sm-12 text-center">
+		                    <h3>People love us.</h3>
+		                </div>
+		            </div>
+		            
+		            <div class="row">
+		                <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+		                    <div class="text-slider slider-arrow-controls text-center relative">
+		                        <ul class="slides">
+		                            <li>
+		                                <p class="lead">LIKE TemecuTech... We Love Them! Ryan has been soo helpful to our business. As Tony would refer to him, He is the Jesse James of computers!</p>
+		                                <div class="quote-author">
+		                                    <h6 class="uppercase mb0 auth">Karen Eby</h6>
+		                                    
+		                                </div>
+		                            </li>
+		                            <li>
+		                                <p class="lead">Temecutech really helped me out when I was in a bind! Fantastic service. Would recommend 100%</p>
+		                                <div class="quote-author">
+		                                    <h6 class="uppercase mb0">Brandon Ireland</h6>
+		                                    <span>Web Designer</span>
+		                                </div>
+		                            </li>
+		                            <li>
+		                                <p class="lead">I can attest to TemecuTech computers professionalism and prompt timing. Currently working with them on several projects. For fast and great pricing.</p>
+		                                <div class="quote-author">
+		                                    <h6 class="uppercase mb0">Aaron Gomez</h6>
+		                                    
+		                                </div>
+		                            </li>
+		                        </ul>
+		                    </div>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section>
+            <section class="bg-secondary recent-news" id="recent-news">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 text-center">
+		                    <h3 class="uppercase color-primary mb40 mb-xs-24">Recent news</h3>
+		                    
+		                </div>
+		            </div>
+		            <div class="row mb64 mb-xs-32">
+		                <div class="col-sm-6">
+		                    <div class="image-caption cast-shadow mb-xs-32 hover-caption">
+                                <a href="http://es.gizmodo.com/instagram-permitira-crear-albumes-con-hasta-10-fotos-1791894574​"> <img alt="Captioned Image" src="img/recent-news2.jpeg"></a>
+		                        <div class="caption">
+		                            <p><b>Instagram will allow you to create photo albums with up to 10 images</b></p>
+		                        </div>
+		                    </div>
+		                </div>
+                        <div class="col-sm-6">
+		                    <div class="image-caption cast-shadow mb-xs-32 hover-caption">
+                                <a href="http://www.vox.com/energy-and-environment/2017/2/7/14533618/solar-jobs-coal​"> <img alt="Captioned Image" src="img/recent-news3.jpg"></a>
+		                        <div class="caption">
+		                            <p><b>There are now twice as many solar jobs as coal jobs in the US</b></p>
+		                        </div>
+		                    </div>
+		                </div>
+                        <div class="col-sm-6">
+		                    <div class="image-caption cast-shadow mb-xs-32 mt24 hover-caption">
+                                <a href="http://tech.co/going-miss-net-neutrality-2017-02"> <img alt="Captioned Image" src="img/recent-news.jpeg"></a>
+		                        <div class="caption">
+		                            <p><b>You’re Really Going to Miss Net Neutrality</b></p>
+		                        </div>
+		                    </div>
+		                </div>
+		                <div class="col-sm-6">
+		                    <div class="image-caption cast-shadow mt24 hover-caption">
+                                <a href="https://www.eff.org/deeplinks/2017/02/yoda-bill-would-let-you-own-and-sell-your-devices-re-introduced-congress"><img alt="Captioned Image" src="img/recent-news1.jpeg"></a>
+		                        <div class="caption">
+		                            <p><b>YODA, the Bill That Would Let You Own (and Sell) Your Devices, Is Re-Introduced in Congress</b></p>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </section><section>
+		        <div class="container contact-us" id="contact-us">
+		            <div class="row" id="form">
+                        <div class="col-sm-6 col-md-5 col-md-offset-1">
+		                    <form method="post" action="mailer.php" class="form-email" data-success="Thanks for your submission, we will be in touch shortly." data-error="Please fill all fields correctly.">
+                               <?php
+                                    if($GET['success'] == 1){
+                                    echo "<div class=\"form-message success\">
+                                    <strong>Thank you! your message has been sent.</strong>
+                                </div>";
+                                        
+                                }
+                                
+                                if($GET['success'] == -1){
+                                    echo "<div class=\"form-message error\">
+                                    <strong>Oops, something went wrong. Please try again!.</strong>
+                                </div>";
+                                    
+                                }
+                                
+                                ?>
+		                        <input type="text" class="validate-required" name="name" id="name" placeholder="Your Name">
+		                        <input type="email" class="validate-required validate-email" name="email" id="email" placeholder="Email Address">
+		                        <textarea class="validate-required" name="message" id="message" rows="4" placeholder="Message"></textarea>
+		                        <button type="submit">Send Message</button>
+		                    </form>
+		                </div>
+		                <div class="col-sm-6 col-md-5">
+		                    <h4 class="uppercase">Get In Touch with us.</h4>
+		                    <p>
+		                        Have any questions? Give us a call. We're always available.</p>
+		                    <hr>
+                            <h5 class="uppercase">Email</h5>
+		                    &nbsp;info@temecutech.com<br><br>
+		                    <hr>
+                            <h5 class="uppercase">Phone number</h5>
+		                    <p>
+		                        (951) 377-1133 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br>
+		                    </p>
+		                </div>
+		            </div>
+		            
+		        </div>
+		        
+		    </section>
+            <footer class="footer-2 bg-dark text-center-xs">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">
+							<a href="index.html"><img class="image-xs fade-half" alt="Pic" src="img/logo2.png"></a>
+						</div>
+					
+						<div class="col-sm-4 text-center">
+							<span class="fade-half">
+								© Copyright 2017 Temecula Computers - All Rights Reserved
+							</span>
+						</div>
+					
+						<div class="col-sm-4 text-right text-center-xs">
+							<ul class="list-inline social-list">
+								<li><a href="https://www.facebook.com/TemecuTech/"><i class="ti-facebook"></i></a></li>
+								
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer></div>
+        <script src="js/js.js"></script>
+        <script src="https://cdn.jsdelivr.net/g/respond@1.4.2,html5shiv@3.7.3,selectivizr@1.0.3b"></script>
+    </body>
+</html>
+				
